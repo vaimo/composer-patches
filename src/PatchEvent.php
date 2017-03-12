37e7ae6528f43d3a8b8e9e7647482d13305de364
Allan Paiste <allan.paiste@vaimo.com>
2017-03-12 18:06:27 +0100

    Code intendation changed in all classes to match the one used by Composer's main app (4 spaces, used to be 2 in the plugin code)

--- src/PatchEvent.php
+++ src/PatchEvent.php
@@ -3,42 +3,42 @@ namespace cweagans\Composer;
 
 class PatchEvent extends \Composer\EventDispatcher\Event
 {
- /**
-  * @var \Composer\Package\PackageInterface $package
-  */
- protected $package;
+    /**
+     * @var \Composer\Package\PackageInterface $package
+     */
+    protected $package;
 
- /**
-  * @var string $url
-  */
- protected $url;
+    /**
+     * @var string $url
+     */
+    protected $url;
 
- /**
-  * @var string $description
-  */
- protected $description;
+    /**
+     * @var string $description
+     */
+    protected $description;
 
-  public function __construct($eventName, \Composer\Package\PackageInterface $package, $url, $description)
-  {
-    parent::__construct($eventName);
+    public function __construct($eventName, \Composer\Package\PackageInterface $package, $url, $description)
+    {
+        parent::__construct($eventName);
 
-    $this->package = $package;
-    $this->url = $url;
-    $this->description = $description;
-  }
+        $this->package = $package;
+        $this->url = $url;
+        $this->description = $description;
+    }
 
-  public function getPackage()
-  {
-    return $this->package;
-  }
+    public function getPackage()
+    {
+        return $this->package;
+    }
 
-  public function getUrl()
-  {
-    return $this->url;
-  }
+    public function getUrl()
+    {
+        return $this->url;
+    }
 
-  public function getDescription()
-  {
-    return $this->description;
-  }
+    public function getDescription()
+    {
+        return $this->description;
+    }
 }
--- src/PatchEvents.php
+++ src/PatchEvents.php
@@ -3,17 +3,17 @@ namespace cweagans\Composer;
 
 class PatchEvents
 {
-  /**
-   * The event listener method receives a cweagans\Composer\PatchEvent instance.
-   *
-   * @var string
-   */
-  const PRE_PATCH_APPLY = 'pre-patch-apply';
+    /**
+     * The event listener method receives a cweagans\Composer\PatchEvent instance.
+     *
+     * @var string
+     */
+    const PRE_PATCH_APPLY = 'pre-patch-apply';
 
-  /**
-   * The event listener method receives a cweagans\Composer\PatchEvent instance.
-   *
-   * @var string
-   */
-  const POST_PATCH_APPLY = 'post-patch-apply';
+    /**
+     * The event listener method receives a cweagans\Composer\PatchEvent instance.
+     *
+     * @var string
+     */
+    const POST_PATCH_APPLY = 'post-patch-apply';
 }
