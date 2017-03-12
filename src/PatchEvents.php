72d4e04c375d783d1953bd2ee6c9b412fec3a530
Allan Paiste <allan.paiste@vaimo.com>
2017-03-12 18:05:28 +0100

    Code styling in other files also changed to match Composer's own styling

--- src/PatchEvents.php
+++ src/PatchEvents.php
@@ -1,17 +1,9 @@
 <?php
-
-/**
- * @file
- * Dispatch events when patches are applied.
- */
-
 namespace cweagans\Composer;
 
-class PatchEvents {
-
+class PatchEvents
+{
   /**
-   * The PRE_PATCH_APPLY event occurs before a patch is applied.
-   *
    * The event listener method receives a cweagans\Composer\PatchEvent instance.
    *
    * @var string
@@ -19,12 +11,9 @@ class PatchEvents {
   const PRE_PATCH_APPLY = 'pre-patch-apply';
 
   /**
-   * The POST_PATCH_APPLY event occurs after a patch is applied.
-   *
    * The event listener method receives a cweagans\Composer\PatchEvent instance.
    *
    * @var string
    */
   const POST_PATCH_APPLY = 'post-patch-apply';
-
 }
