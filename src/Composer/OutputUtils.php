f507de80c5df9d59530946ecc2564ad80e2210a5
Allan Paiste <allan.paiste@vaimo.com>
2017-10-08 20:08:36 +0200

    Unused class removed

--- src/Composer/OutputUtils.php
+++ src/Composer/OutputUtils.php
@@ -1,8 +1,6 @@
 <?php
 namespace Vaimo\ComposerPatches\Composer;
 
-use Composer\DependencyResolver\Operation;
-
 class OutputUtils extends \Composer\IO\ConsoleIO
 {
     static public function resetVerbosity(\Composer\IO\ConsoleIO $io, $verbosity)
