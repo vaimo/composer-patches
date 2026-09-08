ffac250a494b88e8c19af9a75a7b8bcfb5c99176
Vsevolod Kovalchuk <vsevolod.kovalchuk@vaimo.com>
2026-09-08 17:26:26 +0200

    Backported --json output flag for patch:list to release/3

--- src/Composer/Commands/ListCommand.php
+++ src/Composer/Commands/ListCommand.php
@@ -94,6 +94,13 @@ class ListCommand extends \Composer\Command\BaseCommand
             InputOption::VALUE_NONE,
             'Use latest information from package configurations in vendor folder'
         );
+
+        $this->addOption(
+            '--json',
+            null,
+            InputOption::VALUE_NONE,
+            'Output the list of patches in JSON format'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -191,7 +198,15 @@ class ListCommand extends \Composer\Command\BaseCommand
             ));
         }
 
+        if ($input->getOption('json')) {
+            $output->writeln(json_encode($patches));
+
+            return 0;
+        }
+
         $this->generateOutput($output, $patches);
+
+        return 0;
     }
 
     private function createUnfilteredPatchLoaderPool(\Vaimo\ComposerPatches\Composer\Context $composerContext)
